fee9e9a50b56ba03db6070d52b4c67f383bd80be
Berk <berke@comodo.net>
2023-01-29 01:46:03 +0300

    first commit

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="container text-white"></main>
+  <div class="pt-4 mb-8 px-5 relative">
+    <input type="text" placeholder="Search for a city or state" id="searchBar"
+    class="pt-2 px-2 text-center w-full bg-transparent border-b focus:border-weather-secondary
+    focus:outline-none focus:shadow-md">
+  </div>
+</template>
+<script setup>
+
+</script>
